4632849dbc43a9b2dc77be9226d7992bcd99af09
Zurubabel <brunocft@hotmail.com>
2018-01-28 10:03:02 -0200

    Aulas  + Anotações

---
+++ aula 11/Python/Aula_11.py
@@ -0,0 +1,15 @@
+#Aula 11 - Criando os algorítimos de machine learning (Método FOR)
+
+# 1 - Importar a biblioteca numpy
+import numpy as np
+
+#2 - Criar os vetores dos x
+xp = Empty
+xt = Empty
+
+#3 - Criar os vetores de w
+wp = Empty
+wt = Empty
+
+#4 - Obter o resultado
+
